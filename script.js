08bac2cdb5ef45f324d6103eff80d5bac1fe6569
Nitheesh-Git <nitheeshyadav007@gmail.com>
2025-06-17 16:41:51 +0530

    This has been done

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const grids = document.querySelector(".grids");
-const containerSize = 550;
+const containerSize = 600;
 
 function createGrid(gridsize = 36){
     for(let i=0;i<gridsize*gridsize;i++){
@@ -20,6 +20,7 @@ generate.addEventListener("click",()=>{
     console.log(gridsize)
     grids.innerHTML = ''
     createGrid(gridsize)
+    checkbox.checked = false
 })
 
 
@@ -29,10 +30,53 @@ border.addEventListener("click",()=>{
     const cells = document.querySelectorAll(".cell");
     cells.forEach(cell=>{
         if (checkbox.checked){
-        cell.style.border = "1px solid black"
+        cell.style.border = "1px solid lightgrey"
         }
         else{
         cell.style.border = "none"
         }
+    })
+})
+
+const clear = document.getElementById("clear")
+clear.addEventListener("click",()=>{
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach(cell=>{
+        cell.style.backgroundColor="white"
+    })
+})
+
+let colorValue ="#000000"
+const color = document.getElementById("colorPicker")
+color.addEventListener("change",()=>{
+    colorValue = color.value
+    console.log(colorValue)
+})
+
+grids.addEventListener("mouseover",(e)=>{
+    if(e.target.matches(".cell")){
+        e.target.style.backgroundColor=colorValue
+    }
+})
+
+let r,g,b,rc,max
+max=255
+
+function randomColor(){
+    r = Math.floor(Math.random()*max)
+    g = Math.floor(Math.random()*max)
+    b = Math.floor(Math.random()*max)
+
+    return `rgb(${r} , ${g} , ${b})`
+}
+
+rc = randomColor()
+
+const random = document.getElementById("random")
+random.addEventListener("click",()=>{
+    grids.addEventListener("mouseover",(e)=>{
+        if(e.target.matches(".cell")){
+            e.target.style.backgroundColor=randomColor()
+        }   
     })
 })
